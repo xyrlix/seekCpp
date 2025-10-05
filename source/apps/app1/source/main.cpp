2b9dc37c9cbc2df5956086ca90be53c681066016
uding.li@qq.com <uding.li@qq.com>
2025-10-05 03:00:33 +0000

    update: format

--- source/apps/app1/source/main.cpp
+++ source/apps/app1/source/main.cpp
@@ -8,7 +8,7 @@ int main() {
 
     // dst < src
     int arr1[5] = {1, 2, 3, 4, 5};
-    memcpy(arr1, arr1 + 1, 3 * sizeof(int));   // 目标地址在源地址之前且有重叠
+    memcpy(arr1, arr1 + 1, 3 * sizeof(int));  // 目标地址在源地址之前且有重叠
     for (int i = 0; i < 5; i++) {
         std::cout << arr1[i] << " ";
     }
@@ -16,7 +16,7 @@ int main() {
 
     // dst > src
     int arr2[5] = {1, 2, 3, 4, 5};
-    memcpy(arr2 + 1, arr2, 3 * sizeof(int));   // 目标地址在源地址之后且有重叠
+    memcpy(arr2 + 1, arr2, 3 * sizeof(int));  // 目标地址在源地址之后且有重叠
     for (int i = 0; i < 5; i++) {
         std::cout << arr2[i] << " ";
     }
